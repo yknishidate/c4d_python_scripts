037900eff7b4a115ff4fc7b8b4c67ee541f4677c
nishidate-yuki <asta18425@gmail.com>
2020-03-30 15:57:00 +0900

    add: create_normalized_spline.py

---
+++ utils/create_normalized_spline.py
@@ -0,0 +1,47 @@
+import c4d
+
+def make_editable(obj, inserted=False):
+    res = c4d.utils.SendModelingCommand(command=c4d.MCOMMAND_MAKEEDITABLE, list=[obj])
+    if not res:
+        print "ERROR: make_editable"
+        return False
+    return res[0]
+
+def copy_coodinates(from_obj, to_obj):
+    to_obj.SetMg(from_obj.GetMg())
+
+def main():
+    spline = doc.GetActiveObject()
+
+    doc.StartUndo()
+
+    if not spline.CheckType(c4d.Ospline):
+        print "this is NOT Oline"
+        return
+
+    count = spline.GetPointCount()
+    matrix = spline.GetMg()
+    spline.SetMg(c4d.Matrix())
+
+    mospl = c4d.BaseObject(440000054) # MoSpline
+    mospl[c4d.MGMOSPLINEOBJECT_MODE] = 1 # spline
+    mospl[c4d.MGMOSPLINEOBJECT_SPLINE_MODE] = 2 # even
+    mospl[c4d.MGMOSPLINEOBJECT_SOURCE_SPLINE] = spline
+    mospl[c4d.MGMOSPLINEOBJECT_SPLINE_COUNT] = count
+    mospl.SetName(spline.GetName())
+    mospl.SetMg(matrix)
+
+    doc.InsertObject(mospl)
+    res = make_editable(mospl)
+    res.InsertAfter(spline)
+    doc.AddUndo(c4d.UNDOTYPE_NEW, mospl)
+
+    c4d.EventAdd()
+
+    doc.SetActiveObject(mospl,1)
+
+    doc.EndUndo()
+
+if __name__ == '__main__':
+    main()
+    c4d.EventAdd()
